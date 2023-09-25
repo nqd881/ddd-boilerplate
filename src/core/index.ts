83f88ef07af5345ce05a1a29fd9ffe3661c2d2fb
nqd881 <nqd881@gmail.com>
2023-09-26 00:39:36 +0700

    export core/props-envelope in index

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,5 +3,6 @@ export * from './domain-event';
 export * from './entity';
 export * from './enumeration';
 export * from './value-object';
+export * from './props-envelope';
 
 export * from './interfaces';
